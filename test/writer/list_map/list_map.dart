d9a17fb10c2d5ef9d4b004ece1d857f99c18f001
Lejard Hadrien <hadrien.lejard@gmail.com>
2018-02-03 16:48:44 +0100

    format

--- test/writer/list_map/list_map.dart
+++ test/writer/list_map/list_map.dart
@@ -5,7 +5,6 @@ import 'package:test/test.dart';
 import 'package:jaguar_serializer_cli/src/parser/parser.dart';
 import 'package:jaguar_serializer_cli/src/writer/writer.dart';
 
-
 void main() {
   // todo test generator
 }
